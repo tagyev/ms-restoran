937f688baa61925bf37c3dbfe09fd7b0c8250648
tagyev <allahverditagyev@gmail.com>
2025-10-07 20:31:46 +0400

    task2

--- src/main/java/com/example/msrestoran/dao/repository/RestoranRepositroy.java
+++ src/main/java/com/example/msrestoran/dao/repository/RestoranRepositroy.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface RestoranRepositroy extends JpaRepository<RestoranEntity,Long> {
-//    @Query(value = "SELECT r.id FROM restorans r WHERE r.status IN ('ACTIVE','DELETED')", nativeQuery = true)
+    @Query(value = "SELECT r.id FROM restorans r WHERE r.status IN ('ACTIVE','DELETED')", nativeQuery = true)
     List<Long> findAllByStatusIn();
 }
